e7f453b4e0e69a9cebecf73d4a5bae5ef707e0a5
Wandile <ST10448895@imconnect.edu.za>
2024-06-25 23:53:30 +0300

    Merge branch 'main' of https://github.com/ST10448895/ST10448895-PROG5121-POE
    # Conflicts:
    #	nbproject/project.properties
    #	src/login/Login2.java
    #	src/login/TO_DO_LIST.java
    #	src/login/Task.java
    #	test/login/TO_DO_LISTTest.java

--- src/login/Task.java
+++ src/login/Task.java
@@ -5,72 +5,118 @@
  */
 package login;
 
+import java.util.ArrayList;
+import java.util.List;
 import login.TO_DO_LIST;
 import javax.swing.JOptionPane;
 
-
 public class Task {
 
-    public static String description, task_NAME, taskID, Status,developer_Name;
-    public static int loops, Tasknumber, duration, option;
+    public static List<String> description, task_NAME, taskID, Status, developer_Name;
+    public static int loops, Tasknumber, duration, option, numTasks;
     public static int totals = 0;
 
     public static void main(String[] args) {
-       LoginDetail login = new LoginDetail();
-        Login2 sign = new Login2();
-        
-        login.start();
-        
-        JOptionPane.showMessageDialog(null,"Welcome to EasyKanban" );
-        TO_DO_LIST task;
-        task = getTasks();
+
+        JOptionPane.showMessageDialog(null, "Welcome to EasyKanban");
+
+        // Initialize array lists based on the number of tasks
+        description = new ArrayList<>(numTasks);
+        task_NAME = new ArrayList<>(numTasks);
+        taskID = new ArrayList<>(numTasks);
+        Status = new ArrayList<>(numTasks);
+        developer_Name = new ArrayList<>(numTasks);
+
+        TO_DO_LIST task = new TO_DO_LIST();
+
+        getTasks(task);
     }
 
-    public static TO_DO_LIST getTasks() {
-        TO_DO_LIST task = new TO_DO_LIST();
+    public static void getTasks(TO_DO_LIST task) {
         Tasknumber = 0;
-        while (option != 3) {
-            option = Integer.parseInt(JOptionPane.showInputDialog("Welcome to EasyKanban" + '\n' + "Choose an option" + '\n' + "OPTION1): Add task" + '\n' + "OPTION2): show report" + '\n' + "OPTION3): EXIT"));
+        option = 0;
+        while (option != 7) {
+            option = Integer.parseInt(JOptionPane.showInputDialog("Welcome to EasyKanban" + '\n' + "Choose an option" + '\n' + "1: Add task" + '\n' + "2: Show report" + '\n' + "3: search tasks by Dev" + '\n' + "4: Display done tasks" + '\n' + "5: Search Task by TaskName" + '\n' + "6: Delete a task" + '\n' + "7: EXIT"));
             switch (option) {
                 case 1:
-//the user’s input for the number of tasks they want to enter and converts it to an integer value stored in the loops variable.
-                    loops = Integer.parseInt(JOptionPane.showInputDialog("please enter how many tasks you want to eneter"));
-                    for (int i = 0; i < loops; i++) {//this for loop iterates from 0 up to (but not including) the value stored in loops,
-                        
-                         task.setTaskNO(Tasknumber);
-                        developer_Name = JOptionPane.showInputDialog("Enter developer's name and surname");
-                        task.setDeev_Name(developer_Name);
-                        task.getDev_Name();
-                        task_NAME = JOptionPane.showInputDialog("Enter the name of new task");
-                        task.setTask_Name(task_NAME);
-                        task.getTask_Name();
-                        description = JOptionPane.showInputDialog("Enter task description (less than 50 characters):");
-                        task.description = description;  // Set the description in the task object
-                        task.checkTaskDescription();
-                        task.status();
-                        task.TaskID();
-                        duration = Integer.parseInt(JOptionPane.showInputDialog("Please enter total duration of task in hours"));
-                        task.setDuration(duration);
-                        totals = task.Printtotal(duration);
-                        task.TaskDetails();
-                                         
+                    numTasks = Integer.parseInt(JOptionPane.showInputDialog("Enter the maximum number of tasks you want to manage:"));
+                    loops = numTasks;
+                    for (int i = 0; i < loops; i++) {
+                        if (Tasknumber >= numTasks) {
+                            JOptionPane.showMessageDialog(null, "Maximum number of tasks reached.", "Error", JOptionPane.ERROR_MESSAGE);
+                            break;
+                        }
+
                         Tasknumber++;
+                        task.setTaskNO(Tasknumber);
+                        String devName = JOptionPane.showInputDialog("Enter developer's name and surname");
+                        developer_Name.add(devName);
+                        task.setDev_Name(devName);
+                        task.getDev_Name(Tasknumber - 1);
+                        String taskName = JOptionPane.showInputDialog("Enter the name of new task");
+                        task_NAME.add(taskName);
+                        task.setTask_Name(taskName);
+                        task.getTask_Name(Tasknumber - 1);
+                        String taskDescription = JOptionPane.showInputDialog("Enter task description (less than 50 characters):");
+                        description.add(taskDescription);
+                        task.description.add(taskDescription);
+                        boolean validDescription = task.checkTaskDescription(task.description.size() - 1);
+                        while (!validDescription) {
+                            taskDescription = JOptionPane.showInputDialog("Description too long. Enter task description (less than 50 characters):");
+                            description.set(description.size() - 1, taskDescription);
+                            task.description.set(task.description.size() - 1, taskDescription);
+                            validDescription = task.checkTaskDescription(task.description.size() - 1);
+                        }
+                        String statusOption = JOptionPane.showInputDialog("Choose Task status" + '\n' + "1: TO DO" + '\n' + "2: Done" + '\n' + "3: Doing");
+                        task.status(task.Status.size(), statusOption);
+                        task.TaskID(Tasknumber - 1);
+                        duration = Integer.parseInt(JOptionPane.showInputDialog("Please enter total duration of task in hours"));
+                        task.setDuration(duration, Tasknumber - 1);
+                        totals = task.Printtotal(duration);
+                        task.TaskDetails(Tasknumber - 1);
                     }
-                    while(true){
-                    JOptionPane.showMessageDialog(null," The total task hours is"+'\n'+ task.totals);
-                    
-                    break;
-                    }
+                    System.out.println(task.totals);
+                    String largestTask = task.getTaskWithLargestDuration();
+                    JOptionPane.showMessageDialog(null, largestTask);
                     break;
                 case 2:
-                    JOptionPane.showMessageDialog(null, "Coming soon.....");
+                    String allTasks = task.displayAllTasks();
+                    JOptionPane.showMessageDialog(null, allTasks);
                     break;
                 case 3:
+                    String developer_Task = JOptionPane.showInputDialog("Enter the developer's name to view their tasks:");
+                    String tasks = task.manageTasksByDeveloper(developer_Task);
+                    JOptionPane.showMessageDialog(null, tasks);
+                    break;
+                case 4:
+                    String DoneTasks = task.searchAndDisplayDoneTasks();
+                    JOptionPane.showMessageDialog(null, DoneTasks);
+
+                    break;
+                case 5:
+                    String seraching = JOptionPane.showInputDialog("Enter the task name to search:");
+                    String search_outcome = task.searchTaskByName(seraching);
+                    JOptionPane.showMessageDialog(null, search_outcome);
+                    break;
+                case 6:
+                    String deleteOption = JOptionPane.showInputDialog("Do you want to delete any task? (yes/no)");
+                    if (deleteOption.equalsIgnoreCase("yes")) {
+                        int taskToDelete = Integer.parseInt(JOptionPane.showInputDialog("Enter the task number to delete:")) - 1;
+                        if (taskToDelete >= 0 && taskToDelete < task_NAME.size()) {
+                            task.deleteTask(taskToDelete);
+                            JOptionPane.showMessageDialog(null, "Task " + (taskToDelete + 1) + " has been deleted.");
+                        } else {
+                            JOptionPane.showMessageDialog(null, "Invalid task number.", "Error", JOptionPane.ERROR_MESSAGE);
+                        }
+                    }
+                    break;
+                case 7:
                     JOptionPane.showMessageDialog(null, "Quitting.....");
                     break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Invalid option. Please choose 1, 2,3,4, 5 or 6.", "Error", JOptionPane.ERROR_MESSAGE);
+                    break;
             }
-
         }
-        return task;
     }
 }
