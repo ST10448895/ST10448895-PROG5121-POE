3cff05fbd41541b224910077da1c5eb7e028274e
Wandile <ST10448895@imconnect.edu.za>
2024-06-25 23:53:58 +0300

    Merge branch 'main' of https://github.com/ST10448895/ST10448895-PROG5121-POE
    # Conflicts:
    #	nbproject/project.properties
    #	src/login/Login2.java
    #	src/login/TO_DO_LIST.java
    #	src/login/Task.java
    #	test/login/TO_DO_LISTTest.java

--- src/login/TO_DO_LIST.java
+++ src/login/TO_DO_LIST.java
@@ -5,118 +5,223 @@
  */
 package login;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 //import static to_do_list.Task.option;
 public class TO_DO_LIST {
 
-    public static String description, task_NAME, taskID, Developer_Name, Status, status2;
-    public int loops, Tasknumber, duration, total, opt1, opt2, opt3, status;
-    public int[] array = {10, 12, 55, 11, 1};
+   
+    public List<String> description, task_NAME, taskID, Developer_Name, Status, status2;
+    public List<Integer> Tasknumber, duration;
     public int totals = 0;
+    public String statusOption;
 
-    public static void main(String[] args) {
-
+    public TO_DO_LIST() {
+        description = new ArrayList<>();
+        task_NAME = new ArrayList<>();
+        taskID = new ArrayList<>();
+        Developer_Name = new ArrayList<>();
+        Status = new ArrayList<>();
+        status2 = new ArrayList<>();
+        Tasknumber = new ArrayList<>();
+        duration = new ArrayList<>();
     }
 
-    public void setDeev_Name(String DEV_Name) {
-        Developer_Name = DEV_Name;
+    public void setDev_Name(String DEV_Name) {
+        Developer_Name.add(DEV_Name);
     }
 
-    public String getDev_Name() {
-        // asking for developers name usinga dialog box
-
-        return Developer_Name;
+    public String getDev_Name(int index) {
+        return Developer_Name.get(index);
     }
 
-    public void setTaskNO(int TaskNo) {// accepts an integer parameter named TaskNo.
-        Tasknumber = TaskNo;//this line assigns the value of the TaskNo parameter to a variable 
+    public void setTaskNO(int TaskNo) {
+        Tasknumber.add(TaskNo);
     }
 
-    public int getTaskNO() {
-        return Tasknumber;
+    public int getTaskNO(int index) {
+        return Tasknumber.get(index);
     }
 
     public void setTask_Name(String Name_Of_Task) {
-        task_NAME = Name_Of_Task;
+        task_NAME.add(Name_Of_Task);
     }
 
-    public String getTask_Name() {
-        //asks for the name of tasks
-
-        return task_NAME; //returns task name
+    public String getTask_Name(int index) {
+        return task_NAME.get(index);
     }
 
-    public String status() {//shows a dialog to the user with different task status options 
-        status2 = JOptionPane.showInputDialog("Choose Task status" + '\n' + "Status 1): TO DO" + '\n' + "Status2 ): Done" + '\n' + "Status 3) : Doing");
-        switch (status2) {//the value of status2 is equal to "1,2 or 3", the code inside the case block will be executed.
-            case "1":// if 1 is chosen the status to do is stored and printed
-                String Status1 = "TO DO";
-
-                Status = Status1;
-                System.out.println(Status1);
+    public String status(int index, String statusOption) {
+        String status;
+        switch (statusOption) {
+            case "1":
+                status = "TO DO";
                 break;
-            case "2":// if 2 is chosen the status done is stored and printed
-                String Status2 = "Done!";
-
-                Status = Status2;
-                System.out.println(Status2);
+            case "2":
+                status = "Done!";
                 break;
-            case "3":// if 3 is chosen the status doing is stored and printed
-                String Status3 = "Doing";
-
-                Status = Status3;
-                System.out.println(Status3);
+            case "3":
+                status = "Doing";
                 break;
+            default:
+                status = "Invalid";
         }
-        return Status;
+        if (index < Status.size()) {
+            Status.set(index, status);
+        } else {
+            Status.add(status);
+        }
+
+        return status;
     }
 
-    public boolean checkTaskDescription() {
-
+    public boolean checkTaskDescription(int index) {
         while (true) {
-//a loop that repeatedly prompts the user to enter a task description. 
-            // description = JOptionPane.showInputDialog(null, "Enter a task description less than 50 characters:");
-            if (description.length() > 50) {// If the description is too long, an error message is shown
-                JOptionPane.showMessageDialog(null, "The description is too long. Please enter a description less than 50 characters.", "Error", JOptionPane.ERROR_MESSAGE);
-            } else {//otherwise, a success message is displayed, and the loop terminates.
-                System.out.println("Task successfully captured");
-                break;
+            while (true) {
+                if (description.get(index).length() > 50) {
+                    return false;
+                } else {
+                    System.out.println("Task successfully captured");
+                    break;
+                }
             }
+            return true;
         }
-        return true;
     }
 
-    public String TaskID() {
-//code makes a taskID by combining the first two characters of task_NAME, the Tasknumber, and the last three characters of Developer_Name.
-        taskID = task_NAME.substring(0, 2) + ":" + Tasknumber + ":" + Developer_Name.substring(Developer_Name.length() - 3);
-
-        return taskID;
+    public String TaskID(int index) {
+        String id = task_NAME.get(index).substring(0, 2).toUpperCase() + ":" + Tasknumber.get(index) + ":" + Developer_Name.get(index).substring(Developer_Name.get(index).length() - 3).toUpperCase();
+        taskID.add(id);
+        
+        return id;
     }
 
-    public void setDuration(int time) {
-        duration = time;
+    public void setDuration(int time, int index) {
+        duration.add(time);
     }
 
     public Integer TotalHours() {
-        
-       
-        totals = + duration;//adds the parsed duration value to an existing variable 
-
-        return duration;
-    }
-
-    public int Printtotal(int hour) {
-       
-         totals += hour;//adds the parsed duration value to an existing variable
+        totals = 0; // reset totals to avoid double counting
+        for (int time : duration) {
+            totals += time;
+        }
         return totals;
     }
 
-    public String TaskDetails() {
-        //displays a message dialog with task-related details, including status, developer information, task number, name, description, ID, and duration.
-        JOptionPane.showMessageDialog(null, "Task Status: " + Status + '\n' + "Developer details:  " + Developer_Name + '\n' + "Task number:  " + Tasknumber + '\n' + "Task Name:  " + task_NAME + '\n' + "Task description:  " + description + '\n' + "Task ID :  " + TaskID() + '\n' + "Task Duration: " + duration + "Hours");
+    public int Printtotal(int hour) {
+        totals += hour;
+        return totals;
+    }
 
+    public String TaskDetails(int index) {
+        JOptionPane.showMessageDialog(null, "Task Status: " + Status.get(index) + '\n' + "Developer details: " + Developer_Name.get(index) + '\n' + "Task number: " + Tasknumber.get(index) + '\n' + "Task Name: " + task_NAME.get(index) + '\n' + "Task description: " + description.get(index) + '\n' + "Task ID: " + TaskID(index) + '\n' + "Task Duration: " + duration.get(index) + " Hours");
         return "";
     }
 
+    public String manageTasksByDeveloper(String developer) {
+        StringBuilder tasks = new StringBuilder();
+        boolean taskFound = false;
+
+        for (int i = 0; i < Developer_Name.size(); i++) {
+            if (Developer_Name.get(i).equalsIgnoreCase(developer)) {
+                tasks.append("Task ").append(i + 1).append(": ").append(task_NAME.get(i))
+                        .append("\nDescription: ").append(description.get(i))
+                        .append("\nStatus: ").append(Status.get(i)).append("\n\n");
+                taskFound = true;
+            }
+        }
+
+        if (!taskFound) {
+            return "No tasks found for developer: " + developer;
+        } else {
+            return tasks.toString();
+        }
+    }
+
+    public void deleteTask(int index) {
+        if (index >= 0 && index < Tasknumber.size()) {
+            Tasknumber.remove(index);
+            Developer_Name.remove(index);
+            task_NAME.remove(index);
+            description.remove(index);
+            duration.remove(index);
+            Status.remove(index);
+        } else {
+            System.out.println("Invalid index for deletion: " + index);
+        }
+    }
+
+     public String displayAllTasks() {
+        StringBuilder tasks = new StringBuilder();
+        for (int i = 0; i < task_NAME.size(); i++) {
+            tasks.append("Task ").append(i + 1).append(": ").append(task_NAME.get(i))
+                    .append("\nDescription: ").append(description.get(i))
+                    .append("\nStatus: ").append(Status.get(i))
+                    .append("\nDeveloper: ").append(Developer_Name.get(i))
+                    .append("\nTask ID: ").append(taskID.get(i))
+                    .append("\nDuration: ").append(duration.get(i)).append(" hours\n\n");
+        }
+        return tasks.toString();
+    }
+
+    public String getTaskWithLargestDuration() {
+
+        int LongestDuration = 0;
+        for (int i = 1; i < duration.size(); i++) {
+            if (duration.get(i) > duration.get(LongestDuration)) {
+                LongestDuration = i;
+            }
+        }
+
+        return "Task with the largest duration:\n"
+                + "Task " + (LongestDuration + 1) + " : " + task_NAME.get(LongestDuration)
+                + "\nDescription: " + description.get(LongestDuration)
+                + "\nStatus: " + Status.get(LongestDuration)
+                + "\nDeveloper: " + Developer_Name.get(LongestDuration)
+                + "\nTask ID: " + taskID.get(LongestDuration)
+                + "\nDuration: " + duration.get(LongestDuration) + " hours\n";
+    }
+
+    public String searchTaskByName(String taskName) {
+        StringBuilder search = new StringBuilder();
+        boolean find = false;
+
+        for (int i = 0; i < task_NAME.size(); i++) {
+            if (task_NAME.get(i).equalsIgnoreCase(taskName)) {
+                search.append("Task ").append(i + 1).append(" : ").append(task_NAME.get(i))
+                        .append("\nStatus : ").append(Status.get(i))
+                        .append("\nDeveloper : ").append(Developer_Name.get(i)).append("\n\n");
+                find = true;
+            }
+        }
+        if (!find) {
+            return "No task found with the name: " + taskName;
+        }
+
+        return search.toString();
+    }
+
+    public String searchAndDisplayDoneTasks() {
+        StringBuilder done = new StringBuilder();
+        boolean doneTaskFound = false;
+
+        for (int i = 0; i < Status.size(); i++) {
+            if (Status.get(i).equals("Done!")) {
+                done.append("Task ").append(i + 1).append(": ").append(task_NAME.get(i))
+                        .append("\nDescription: ").append(description.get(i))
+                        .append("\nDeveloper: ").append(Developer_Name.get(i))
+                        .append("\nTask ID: ").append(taskID.get(i))
+                        .append("\nDuration: ").append(duration.get(i)).append(" hours\n\n");
+                doneTaskFound = true;
+            }
+        }
+
+        if (!doneTaskFound) {
+            return "No tasks with status 'Done!' found.";
+        }
+
+        return done.toString();
+    }
 }
